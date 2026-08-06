7ea6613177b0597762f454b1b9b1217dddba671f
createctoco <admin@ctoco.com>
2026-08-06 23:50:23 +0800

    Prepare locale build foundation: src/utils/permalinks.ts

--- src/utils/permalinks.ts
+++ src/utils/permalinks.ts
@@ -3,6 +3,7 @@ import slugify from 'limax';
 import { SITE, APP_BLOG } from 'astrowind:config';
 
 import { trim } from '~/utils/utils';
+import { BUILD_SITE_URL } from '~/i18n/config';
 
 export const trimSlash = (s: string) => trim(trim(s, '/'));
 const createPath = (...params: string[]) => {
@@ -29,7 +30,7 @@ export const POST_PERMALINK_PATTERN = trimSlash(APP_BLOG?.post?.permalink || `${
 
 /** */
 export const getCanonical = (path = ''): string | URL => {
-  const url = String(new URL(path, SITE.site));
+  const url = String(new URL(path, BUILD_SITE_URL || SITE.site));
   if (SITE.trailingSlash == false && path && url.endsWith('/')) {
     return url.slice(0, -1);
   } else if (SITE.trailingSlash == true && path && !url.endsWith('/')) {
